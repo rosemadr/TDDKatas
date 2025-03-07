b625e839306bdc2b5aa661bcd303924544b7e68f
madi.simcock-brown <madi.simcock-brown@defra.onmicrosoft.com>
2025-03-07 16:35:39 +0000

    fixed requirement 1

--- src/main/java/org/example/exercises/FizzBuzz.java
+++ src/main/java/org/example/exercises/FizzBuzz.java
@@ -1,11 +1,17 @@
 package org.example.exercises;
 
+
+
 public class FizzBuzz {
 
   public String fizzBuzz(int number) {
     String result = "";
     if (number % 3 == 0) {
       result = "Fizz";
+    } else if (number % 5 == 0) {
+      return "Buzz";
+    } else {
+      return String.valueOf(number);
     }
     return result;
   }
--- src/test/java/org/example/exercises/FizzBuzzTest.java
+++ src/test/java/org/example/exercises/FizzBuzzTest.java
@@ -7,10 +7,24 @@ import org.example.exercises.FizzBuzz;
 
 public class FizzBuzzTest {
 
-    @Test
-    public void testfizzBuzz() {
+  @Test
+  public void testToString() {
+    FizzBuzz testFizzBuzz = new FizzBuzz();
+    assertEquals("2", testFizzBuzz.fizzBuzz(2));
+  }
+
+
+  @Test
+  public void testFizz() {
       FizzBuzz testFizzBuzz = new FizzBuzz();
       assertEquals("Fizz", testFizzBuzz.fizzBuzz(9));
   }
+
+  @Test
+  public void testBuzz() {
+    FizzBuzz testFizzBuzz = new FizzBuzz();
+    assertEquals("Buzz", testFizzBuzz.fizzBuzz(25));
+  }
+
 
 }
