f6760a295d51e78abdcb08e312d28e8f81a3e80d
madi.simcock-brown <madi.simcock-brown@defra.onmicrosoft.com>
2025-03-07 15:55:51 +0000

    made classes

---
+++ src/main/java/org/example/exercises/FizzBuzz.java
@@ -0,0 +1,5 @@
+package org.example.exercises;
+
+public class FizzBuzz {
+
+}
---
+++ src/test/java/org/example/exercises/FizzBuzzTest.java
@@ -0,0 +1,15 @@
+package org.example.exercises;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    void testfizzBuzz() {
+      assertEquals('Fizzbuzz', 15);
+
+  }
+
+
+
+}
