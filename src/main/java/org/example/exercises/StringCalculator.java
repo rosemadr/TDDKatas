6fa6acf4243dabc2bdcd6ef7451b09703f9d80a4
madi.simcock-brown <madi.simcock-brown@defra.onmicrosoft.com>
2025-03-14 18:10:46 +0000

    up to handling separators at the end of strings

--- src/main/java/org/example/exercises/StringCalculator.java
+++ src/main/java/org/example/exercises/StringCalculator.java
@@ -1,22 +1,48 @@
 package org.example.exercises;
 
+import java.util.*;
+
 public class StringCalculator {
 
-  public int add(String string) {
-    int result = 9999;
+  public int add(String... strings) {
+    int sum = 0;
+    for (String string : strings) {
+      sum += sumIndividualNumbers(string);
+    }
+    return sum;
+  }
+
+  public int sumIndividualNumbers(String string) {
     if (string.isEmpty()) {
-      result = 0;
-    } else if (string.contains(",")) {
+      return 0;
+    } else if (string.)
+
+    else if (string.contains(",")) {
       String[] splitString = string.split(",");
-      int[] intsList;
-      for (int i = 0; i < splitString.size(); i++) {
-        intsList.append(Integer.parseInt(i));
-      }
+      return handleCommas(string);
+    } else if (string.contains("\n")) {
+      return handleNewlines(string);
+    } else {
+      return Integer.parseInt(string);
     }
-    {
-      result = Integer.parseInt(string);
+  }
+
+  public int handleCommas(String string) {
+    int sum = 0;
+    String[] splitString = string.split(",");
+    for (String s : splitString) {
+      sum += Integer.parseInt(s);
     }
-    return result;
+    return sum;
+  }
+
+  public int handleNewlines(String string) {
+    int sum = 0;
+    String[] splitString = string.split("\n");
+    for (String s : splitString) {
+      sum += Integer.parseInt(s);
+    }
+    return sum;
   }
 
 }
--- src/test/java/org/example/exercises/StringCalculatorTest.java
+++ src/test/java/org/example/exercises/StringCalculatorTest.java
@@ -24,4 +24,27 @@ public class StringCalculatorTest {
     StringCalculator stringCalc = new StringCalculator();
     assertEquals(18, stringCalc.add("6,12"));
   }
+
+  @Test
+  public void testMultipleArguments() {
+    StringCalculator stringCalc = new StringCalculator();
+    assertEquals(28, stringCalc.add("1,2,3,4,5", "0", "4", "4,5"));
+  }
+
+  @Test
+  public void testHandleNewlineSeparators() {
+    StringCalculator stringCalc = new StringCalculator();
+    assertEquals(10, stringCalc.add("1\n2\n3\n4"));
+  }
+
+  @Test
+  public void testSeparatorAtEndThrowsException() {
+    StringCalculator stringCalc = new StringCalculator();
+    try {
+      stringCalc.add("1,");
+      fail("Expected an exception to be thrown");
+    } catch (IllegalArgumentException e) {
+      assertEquals("Number expected but EOF found", e.getMessage());
+    }
+  }
 }
